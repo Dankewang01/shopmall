83485c78ab854e2752cb1220276c5c17b20de63c
e <892017222@qq.com>
2019-06-09 17:11:25 +0800

    forms_example

--- shoppingmall_app/admin.py
+++ shoppingmall_app/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
-
+from shoppingmall_app.models import Author, Book, BookType
 # Register your models here.
+
+admin.site.site_header = "失恋博物馆"
+admin.site.site_title = "治愈空间"
+admin.site.index_title = "有你的回忆"
+
+class BookAdmin(admin.ModelAdmin):
+   # fields = ['name', 'pubdate']  # 只显示name price 字段信息
+    list_per_page = 2
+    actions_on_buttom = True
+    actions_on_top = False
+    list_display = ["name", "curTime", "getAuthorName"]  # 显示数据库字段
+    search_fields = ["name"]   #搜索选框，搜索NAME字段
+
+  
+
+admin.site.register(Book,BookAdmin)
+
+
+'''
+#@admin.register(Book) #注册装饰器写法
+class BookAdmin(admin.ModelAdmin):
+    list_per_page = 2
+    actions_on_buttom = True
+    actions_on_top = False
+    list_display =[ "name", "curTime", "getAuthorName"]#显示数据库字段
+    search_fields = ["name"]  #搜索选框，搜索NAME字段
+
+    fields = ['name' , 'pubdate']#只显示name price 字段信息
+
+    #分组显示
+    fieldssets=(
+        ('基本信息',{'fields':['name',]}),
+        ('其他信息',{'fields':['price',]}),
+    )
+
+admin.site.register(Book,BookAdmin)
+'''
+
+#@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "age"]  # 显示数据库字段
+    search_fields = ["name"]  # 搜索选框，搜索NAME字段
+
+admin.site.register(Author, AuthorAdmin)
+
+
+#@admin.register(BookType)
+class BookTypeAdmin(admin.ModelAdmin):
+    list_display = ["caption"]  # 显示数据库字段
+    search_fields = ["caption"]  # 搜索选框，搜索NAME字段
+    list_per_age = 2
+
+admin.site.register(BookType,BookTypeAdmin)
+
+
+
+
+
--- shoppingmall_app/models.py
+++ shoppingmall_app/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-
+import time
 # Create your models here.
 class Author(models.Model):
     """
@@ -9,15 +9,23 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
+
 class BookType(models.Model):
     """
     图书类型
     """
     caption = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.caption
+
+
 class Book(models.Model):
     """
-    图书in
+    图书
     """
     name = models.CharField(max_length=64)
     pages = models.IntegerField()
@@ -26,5 +34,28 @@ class Book(models.Model):
 
     authors = models.ManyToManyField(Author)
     book_type = models.ForeignKey(BookType)
+
+
+
+    def __str__(self):
+       return self.name
+
+    def getAuthorName(self):
+        return self.Author.name
+
+    def curTime(self):
+        return time.time()
+
+    curTime.short_description = "当前时间"
+    curTime.admin_order_feild = "name"  # 设置为以name为准，对函数进行排序
+
+
+
+
+
+
+
+
+
 
 #class BookType(models.Model):
